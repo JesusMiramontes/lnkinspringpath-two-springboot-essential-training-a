f627e2adcd1ddc9c9d0c1f11c7b52ab590d0fc6e
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-31 00:33:46 -0600

    Staff repository

--- src/main/java/com/miramontes/two/linkin/becomespringdev/a/controllers/StaffController.java
+++ src/main/java/com/miramontes/two/linkin/becomespringdev/a/controllers/StaffController.java
@@ -1,23 +1,26 @@
 package com.miramontes.two.linkin.becomespringdev.a.controllers;
 
+import com.miramontes.two.linkin.becomespringdev.a.data.StaffRepository;
 import com.miramontes.two.linkin.becomespringdev.a.services.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/staffs")
+@Controller
+@RequestMapping("/staffs")
 public class StaffController {
-    private final StaffService staffService;
+    private final StaffRepository staffRepository;
 
     @Autowired
-    public StaffController(StaffService staffService) {
-        this.staffService = staffService;
+    public StaffController(StaffRepository staffRepository) {
+        this.staffRepository = staffRepository;
     }
 
     @GetMapping
     public String getAllStaffs(Model model){
-        model.addAttribute("staffs", staffService.getAllStaffMembers());
+        model.addAttribute("staffs", staffRepository.findAll());
         return "staffs";
     }
 }
--- src/main/java/com/miramontes/two/linkin/becomespringdev/a/models/Staff.java
+++ src/main/java/com/miramontes/two/linkin/becomespringdev/a/models/Staff.java
@@ -3,12 +3,23 @@ package com.miramontes.two.linkin.becomespringdev.a.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "EMPLOYEE")
 public class Staff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EMPLOYEE_ID")
     private String id;
+    @Column(name = "FIRST_NAME")
     private String firstName;
+    @Column(name = "LAST_NAME")
     private String lastName;
+    @Column(name = "POSITION")
+    @Enumerated(EnumType.STRING)
     private Position position;
 
     public Staff(String id, String firstName, String lastName, Position position) {
